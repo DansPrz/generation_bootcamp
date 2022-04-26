61db8c453456e78f689d15e736534d427ac1c924
Dan_Prz <danielprz.098@gmail.com>
2022-04-26 14:44:23 -0500

    Pagina Cats4U terminada

---
+++ Bootstrap/assets/js/scrollReveal.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('#imagenes');
+ScrollReveal().reveal('#video');
+ScrollReveal().reveal('#adopta');
+
+ScrollReveal().reveal('#imagenes',{delay: 500});
+ScrollReveal().reveal('#video',{delay: 500});
+ScrollReveal().reveal('#adopta',{delay: 500});
